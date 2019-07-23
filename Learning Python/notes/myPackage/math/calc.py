8a4c15607237bea96acda40e51a3e8e001736f07
dodonmountain <lkh151515@gmail.com>
2019-07-23 16:46:42 +0900

    190723| python Module and Error Handling

---
+++ Learning Python/notes/myPackage/math/calc.py
@@ -0,0 +1,24 @@
+def sum(a, b):
+    """
+    두 수를 더합니다.
+    """
+    return a + b
+
+def sub(a, b):
+    """
+    두 수를 뺍니다.
+    """
+    return a - b
+    
+
+def mul(a, b):
+    """
+    두 수를 곱합니다.
+    """
+    return a * b
+
+def div(a, b):
+    """
+    두 수를 나눕니다.
+    """
+    return False if b == 0 else a / b
