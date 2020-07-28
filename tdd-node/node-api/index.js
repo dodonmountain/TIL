64c7b912e8e1633d596f417ce0faefe4cb0eced2
dodonmountain <lkh151515@gmail.com>
2020-07-28 16:12:34 +0900

    0728 | readFileSync

--- tdd-node/node-api/index.js
+++ tdd-node/node-api/index.js
@@ -1,7 +1,11 @@
 const http = require('http')
 const math = require('./math.js')
+const fs = require('fs')
+
+const data = fs.readFileSync('./data.txt', 'utf-8')
+
 
 const result = math.sum(1, 2)
 
 console.log(result) // 3
-
+console.log(data)
